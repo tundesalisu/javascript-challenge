53e738d036e5188ab28a0635a75cfb8730bea128
Salisu <babatunde.salisu@siemens-healthineers.com>
2020-09-24 00:44:08 -0400

    commiting

--- 14-Intro-To-JavaScript/UFO-level-1/StarterCode/static/js/app.js
+++ 14-Intro-To-JavaScript/UFO-level-1/StarterCode/static/js/app.js
@@ -2,3 +2,15 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// console.log(data);
+var tbody = d3.select("tbody");
+data.forEach((nasa) =>{
+    // console.log(nasa);
+    var row = tbody.append('tr');
+    Object.entries(nasa).forEach(([key,value])=>{
+        console.log(key,value);
+        var cell = row.append("td");
+        cell.text(value);
+    })
+
+})
